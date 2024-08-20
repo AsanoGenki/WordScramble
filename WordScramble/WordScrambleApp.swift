e9536f03d5d7e93ac1558a35d1082ae528094934
AsanoGenki <asanogenki0218@icloud.com>
2024-08-20 20:52:02 +0900

    Initial commit.

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Genki on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
